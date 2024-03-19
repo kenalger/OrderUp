3fc9e8bbd73a9558920d06e900d1f8db7348e397
Ken Alger Dimaymay <111838907+kenalger@users.noreply.github.com>
2024-03-19 19:48:41 +0800

    Add files via upload

---
+++ OrderUp/src/test/java/com/csit332/OrderUp/OrderUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csit332.OrderUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
